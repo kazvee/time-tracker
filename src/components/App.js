d687ab2efc5e0b3d2b37e352612ffd2a322ac887
kazvee <kazveedev@gmail.com>
2024-03-13 10:10:56 -0600

    feat: add state management for date input

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,29 @@ import { Button, Form, FormControl } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newDate: '',
+      date: '2000-01-01',
+    };
+  }
+
+  changeDate() {
+    console.log(`⌚ ${this.state.newDate}`);
+    this.setState({ date: this.state.newDate });
+  }
+
   render() {
     return (
       <div className='App'>
         <Form inline>
           <h2>Input a Date</h2>
-          <FormControl type='date'></FormControl>
-          <Button>Submit</Button>
+          <FormControl
+            type='date'
+            onChange={(event) => this.setState({ newDate: event.target.value })}
+          ></FormControl>
+          <Button onClick={() => this.changeDate()}>Submit</Button>
         </Form>
       </div>
     );
