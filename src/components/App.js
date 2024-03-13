c97f78df2dfa7df460eb2145b571e5e97411658d
kazvee <kazveedev@gmail.com>
2024-03-13 09:33:22 -0600

    feat: create App component

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <div>Time Tracker ⌚</div>;
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-ReactDOM.render(<div>Time Tracker ⌚</div>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
