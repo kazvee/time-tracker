1c4c2a2b61d93ddfdc718d0d77a0ceab9c35030c
kazvee <kazveedev@gmail.com>
2024-03-13 09:59:27 -0600

    style: add styling to Form component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,15 @@
 import { Component } from 'react';
 import { Button, Form, FormControl } from 'react-bootstrap';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='App'>
         <Form inline>
-          <FormControl type='date'>
-          </FormControl>
-          {' '}
-          <Button>
-            Submit
-          </Button>
+          <h2>Input a Date</h2>
+          <FormControl type='date'></FormControl>
+          <Button>Submit</Button>
         </Form>
       </div>
     );
